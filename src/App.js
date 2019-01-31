35cf4f93d7bef6d32587639fa718314ed8e7c168
svenl <sven.lecann@live.fr>
2019-01-31 15:56:56 +0100

    First lesson

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>Hi, i'm a React App</h1>
+       <p>This is realy working</p>
       </div>
     );
+    //return React.createElement('div', {classeName: 'App'}, React.createElement('h1', null, 'Does this work ?'))
   }
 }
 
